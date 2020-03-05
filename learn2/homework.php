b2438016e0be88ac101c5eacfad7b78bad020745
dat <thanhdat112.pl@gmail.com>
2020-03-05 02:39:53 +0100

    coomit

---
+++ learn2/homework.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="header">
+    <h1><u><i>S</i>ear<b>c</b>h <i>B</i>ar<u></h1>
+    <input type="text" name="text" placeholder="Please write smt in here">
+    <p class="p1">If you want to see some PICTURE OF NATURAL please click
+    <a href="https://www.google.com/search?q=picture+nature&rlz=1C1CHBF_plPL861PL861&sxsrf=ACYBGNT6HIwKuGMSCQSc-45R9kAE8Y2bXw:1581288658448&source=lnms&tbm=isch&sa=X&ved=2ahUKEwipr5imx8XnAhURmIsKHQ00Ad0Q_AUoAXoECAwQAw&biw=1366&bih=657" target="_blank">here.</a></p>
+    </div>
+    <div id="body">
+    <h2>ANIMAL</h2>
+    <p>There are so many of<span class="p2"> animals</span> in the nature:</p>
+    <img src="https://www.google.com/search?q=panda&tbm=isch&ved=2ahUKEwig8aKFyMXnAhWXwSoKHQyJD4gQ2-cCegQIABAA&oq=panda&gs_l=img.3..0i67j0l4j0i67j0l3j0i67.134016.135535..135663...1.0..0.113.576.4j2......0....1..gws-wiz-img.......35i39._SOlKAL5VMU&ei=mY1AXuDAM5eDqwGMkr7ACA&bih=657&biw=1366&rlz=1C1CHBF_plPL861PL861" alt="Panda">
+    <br>
+    <img src="https://www.google.com/search?q=elephent&tbm=isch&ved=2ahUKEwiZjcTGyMXnAhVRB3cKHQguDHkQ2-cCegQIABAA&oq=elephent&gs_l=img.3...38075.41249..41554...0.0..0.114.799.4j4......0....1..gws-wiz-img.......0j0i67.mV1mofctwyU&ei=Io5AXpn0KtGO3AOI3LDIBw&bih=657&biw=1366&rlz=1C1CHBF_plPL861PL861" alt="Elephant">
+    <br>
+    <img src="https://www.google.com/search?q=monkey&tbm=isch&ved=2ahUKEwiQuvrayMXnAhXNyyoKHVoyA-YQ2-cCegQIABAA&oq=monkey&gs_l=img.3..0i67j0l9.55136.56744..57520...0.0..0.179.747.2j4......0....1..gws-wiz-img.srzC9Ju5_Y0&ei=TY5AXtDfIM2XqwHa5IywDg&bih=657&biw=1366&rlz=1C1CHBF_plPL861PL861" alt="Monkey">
+    </div>
+    <div>
+    <h2>TREE</h2>
+    <p>There are so many of<span class="p2"> tree</span> in the nature:</p>
+    <img src="https://www.google.com/search?q=Apple+tree&tbm=isch&ved=2ahUKEwjr1-mnx8XnAhWUzyoKHXc4Bw0Q2-cCegQIABAA&oq=Apple+tree&gs_l=img.3..0l8j0i30l2.24650.2077973..2078213...0.0..0.180.468.2j2......0....1..gws-wiz-img.......35i39j0i10i30j0i5i30.ON_2TSSne3I&ei=1YxAXquZNZSfqwH38Jxo&bih=657&biw=1366&rlz=1C1CHBF_plPL861PL861" alt="Apple tree">
+    <br>
+    <img src="https://www.google.com/search?q=coconut+tree&tbm=isch&ved=2ahUKEwjb3IOTz8XnAhXWxSoKHX-5Dz0Q2-cCegQIABAA&oq=coconut+tree&gs_l=img.3..0l10.1042.3753..3916...0.0..0.151.1249.5j7......0....1..gws-wiz-img.......0i67.ZyqnacJ18YM&ei=DZVAXpuiLdaLqwH_8r7oAw&bih=657&biw=1366&rlz=1C1CHBF_plPL861PL861" alt="Coconut tree">
+    <br>
+    <img src="https://www.google.com/search?q=grape+tree&tbm=isch&ved=2ahUKEwjI_b2Vz8XnAhWStCoKHaH2CWQQ2-cCegQIABAA&oq=grape+tree&gs_l=img.3..0j0i30l9.10695.14113..14283...0.0..0.121.918.8j2......0....1..gws-wiz-img.......0i67.YbCjSvin3FI&ei=EpVAXsisNpLpqgGh7aegBg&bih=657&biw=1366&rlz=1C1CHBF_plPL861PL861" alt="grape tree">
+    </div>
+    </div>
+</body>
+</html>
